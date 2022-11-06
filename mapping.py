477d93d8b65929dc2cef08ac434d43a789de97d5
Rayane Badji <rayanebadji2711@gmail.com>
2022-11-06 20:08:50 +0100

    'Ajout du fichier mapping.py'

---
+++ mapping.py
@@ -0,0 +1,10 @@
+
+# Column : integer -> 
+age_labels = { -0.95197 : list(range(18,25)),
+            -0.07854 : list(range(25,35)),
+            0.49788 : list(range(5,45)),
+            1.09449 : list(range(45-55)),
+            1.82213 : list(range(55-65)),
+            2.59171 : list(range(65,100))  }
+
+print(age_labels)
