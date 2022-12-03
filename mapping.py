9cb338075e0a1117d7525854e1d3321b05ed97f9
Rayane Badji <rayanebadji2711@gmail.com>
2022-12-03 17:52:51 +0100

    New Explore and cleaning

--- mapping.py
+++ mapping.py
@@ -1,10 +1,52 @@
 
 # Column : integer -> 
-age_labels = { -0.95197 : list(range(18,25)),
+map_dict_age = { -0.95197 : list(range(18,25)),
             -0.07854 : list(range(25,35)),
             0.49788 : list(range(5,45)),
             1.09449 : list(range(45-55)),
             1.82213 : list(range(55-65)),
             2.59171 : list(range(65,100))  }
 
-print(age_labels)
+map_dict_gender = {0.48246:"Female",
+                   -0.48246:"Male"}
+
+map_dict_education = {-2.43591:"Left school before 16 years",
+                      -1.73790:"Left school at 16 years",
+                      -1.43719:"Left school at 17 years", 
+                      -1.22751:"Left school at 18 years",
+                      -0.61113:"Some college or university, no certificate or degree",
+                      -0.05921:"Professional certificate/ diploma",
+                      0.45468:"University degree", 
+                      1.16365:"Masters degree",
+                      1.98437:"Doctorate degree"}
+
+
+map_dict_country = {-0.09765:"Australia", 
+                    0.24923:"Canada", 
+                    -0.46841:"New Zealand",
+                    -0.28519:"Other", 
+                    0.21128:"Republic of Ireland", 
+                    0.96082:"UK", 
+                    -0.57009:"USA"}
+
+
+map_dict_ethnicity = {-0.50212:"Asian",
+                      -1.10702:"Black", 
+                      1.90725:"Black-Asian", 
+                      0.12600:"White-Asian",
+                      -0.22166:"White-Black",
+                      0.11440:"Other",
+                      -0.31685:"White"}
+
+map_dict_drugs = {'CL0' : 0,
+                    'CL1' : 1,
+                    'CL2' : 2,
+                    'CL3' : 3,
+                    'CL4' : 4,
+                    'CL5' : 5,
+                    'CL6' : 6}
+# A voir si je le garde comme celui des drogues
+
+
+
+
