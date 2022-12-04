abd183ff53bdee50356bc200daa8d060523a3be1
KakizzV2 <71672876+KakizzV2@users.noreply.github.com>
2022-12-04 15:08:39 +0100

    ajout de la partie d'arthur à la partie de rayanne

--- mapping.py
+++ mapping.py
@@ -1,10 +1,59 @@
 
 # Column : integer -> 
-age_labels = { -0.95197 : list(range(18,25)),
-            -0.07854 : list(range(25,35)),
-            0.49788 : list(range(5,45)),
-            1.09449 : list(range(45-55)),
-            1.82213 : list(range(55-65)),
-            2.59171 : list(range(65,100))  }
 
-print(age_labels)
+# mapping 
+
+map_dict_age = { -0.95197 : "[18-24]",
+            -0.07854 : "[25-34]",
+            0.49788 : "[35-45]",
+            1.09449 : "[45-55]",
+            1.82213 : "[55-65]",
+            2.59171 : "[65-100]"}
+
+map_dict_gender = {0.48246:"Female",
+                   -0.48246:"Male"}
+
+map_dict_education = {-2.43591:"Left school before 16 years",
+                      -1.73790:"Left school at 16 years",
+                      -1.43719:"Left school at 17 years", 
+                      -1.22751:"Left school at 18 years",
+                      -0.61113:"Some college or university, no certificate or degree",
+                      -0.05921:"Professional certificate/ diploma",
+                      0.45468:"University degree", 
+                      1.16365:"Masters degree",
+                      1.98437:"Doctorate degree"}
+
+
+map_dict_country = {-0.09765:"Australia", 
+                    0.24923:"Canada", 
+                    -0.46841:"New Zealand",
+                    -0.28519:"Other", 
+                    0.21128:"Republic of Ireland", 
+                    0.96082:"UK", 
+                    -0.57009:"USA"}
+
+
+map_dict_ethnicity = {-0.50212:"Asian",
+                      -1.10702:"Black", 
+                      1.90725:"Black-Asian", 
+                      0.12600:"White-Asian",
+                      -0.22166:"White-Black",
+                      0.11440:"Other",
+                      -0.31685:"White"}
+
+map_dict_drugs = {'CL0' : 'NeverUsed',
+                    'CL1' : 'OverDecadeAgo',
+                    'CL2' : 'LastDecade',
+                    'CL3' : 'LastYear',
+                    'CL4' : 'LastMonth',
+                    'CL5' : 'LastWeek',
+                    'CL6' : 'LastDay'}
+# A voir si je le garde comme celui des drogues
+
+
+# encoding 
+
+
+
+
+
